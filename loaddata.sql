da7c2c884b3f74f27c822c63bb7cbd3337cb7b75
Nora Szeto <neszeto@gmail.com>
2022-10-19 15:56:10 -0500

    minor changes to loaddata.sql

--- loaddata.sql
+++ loaddata.sql
@@ -91,12 +91,17 @@ CREATE TABLE "Categories" (
 INSERT INTO Categories ('label') VALUES ('News');
 INSERT INTO Tags ('label') VALUES ('JavaScript');
 INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
-<<<<<<< HEAD
-=======
+
 
 INSERT INTO `Users` VALUES (null, "Lorin", "Jones", "Lorin.Jones@gmail.com", "Hairbrained. Nuts. Hysterical but Harmless.", "SandwichArtist", "videodrome", "Pigpoop.jpg", 2022-10-18, 1);
 INSERT INTO `Users` VALUES (null, "Nora", "Szeto", "Nora.Szeto@gmail.com", "Master of Education. Don't fuck with me.", "R2Szeto", "operation", "puppy.jpg", 2022-10-19, 1)
 
-SELECT * FROM Users
+SELECT * FROM Users;
 
-INSERT INTO `Posts` VALUES (null, 1, 2, "Parsnip&Pear, new cycle syncing app", 2022-10-19, "https://media-cdn.greatbritishchefs.com/media/ejzcxjnx/img18789.jpg?mode=crop&width=1536&height=1024", "Next week, a new cycle syncing app will be released by Gracie Parce, software developer. Parsnip&Pear is devoted to teaching women about their bodies and how to live more cyclically, aligned with their menstrual cycle.")
+INSERT INTO `Posts` VALUES (null, 1, 2, "Parsnip&Pear, new cycle syncing app", 2022-10-19, "https://media-cdn.greatbritishchefs.com/media/ejzcxjnx/img18789.jpg?mode=crop&width=1536&height=1024", "Next week, a new cycle syncing app will be released by Gracie Parce, software developer. Parsnip&Pear is devoted to teaching women about their bodies and how to live more cyclically, aligned with their menstrual cycle.");
+
+
+INSERT INTO `Tags` VALUES (null, "Treats");
+INSERT INTO `Tags` VALUES (null, "Embalming");
+INSERT INTO `Tags` VALUES (null, "Sacred Rites and Rituals");
+INSERT INTO `Tags` VALUES (null, "Hell on Earth");
