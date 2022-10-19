92b916cb05e4cc9ec4e76c6719cd7e4622a4a7fd
Justin Dority <justindority@gmail.com>
2022-10-19 15:38:23 -0500

    trying to navigate treacherous merge conflicts

--- loaddata.sql
+++ loaddata.sql
@@ -38,7 +38,6 @@ CREATE TABLE "Posts" (
   "publication_date" date,
   "image_url" varchar,
   "content" varchar,
-  
   FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`)
 );
 
@@ -88,14 +87,11 @@ CREATE TABLE "Categories" (
 INSERT INTO Categories ('label') VALUES ('News');
 INSERT INTO Tags ('label') VALUES ('JavaScript');
 INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
-<<<<<<< HEAD
-=======
 
 INSERT INTO `Users` VALUES (null, "Lorin", "Jones", "Lorin.Jones@gmail.com", "Hairbrained. Nuts. Hysterical but Harmless.", "SandwichArtist", "videodrome", "Pigpoop.jpg", 2022-10-18, 1)
 INSERT INTO `Users` VALUES (null, "Nora", "Szeto", "Nora.Szeto@gmail.com", "Master of Education. Don't fuck with me.", "R2Szeto", "operation", "puppy.jpg", 2022-10-19, 1)
 
 SELECT * FROM Users
-<<<<<<< HEAD
 
 INSERT INTO `Tags` VALUES (null, "Treats")
 INSERT INTO `Tags` VALUES (null, "Embalming")
@@ -109,9 +105,5 @@ SELECT id, username
       and password = "videodrome"
 
 SELECT * FROM Users
-=======
->>>>>>> main
 
 INSERT INTO `Posts` VALUES (null, 1, 2, "Parsnip&Pear, new cycle syncing app", 2022-10-19, "https://media-cdn.greatbritishchefs.com/media/ejzcxjnx/img18789.jpg?mode=crop&width=1536&height=1024", "Next week, a new cycle syncing app will be released by Gracie Parce, software developer. Parsnip&Pear is devoted to teaching women about their bodies and how to live more cyclically, aligned with their menstrual cycle.")
-
-DELETE FROM Users WHERE id >0
