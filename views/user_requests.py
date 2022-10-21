790972c872b9683adda99707ea479983e548edba
Justin Dority <justindority@gmail.com>
2022-10-21 10:53:24 -0500

    saving to switch branches for review

--- views/user_requests.py
+++ views/user_requests.py
@@ -109,3 +109,42 @@ def get_all_users():
             users.append(user.__dict__)
 
         return users
+
+def get_single_user(id):
+    """gets a single user
+    
+    Args: id
+    
+    Returns single user dictionary"""
+
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            u.id,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.bio,
+            u.username,
+            u.password,
+            u.profile_image_url,
+            u.created_on,
+            u.active
+        FROM users u
+        WHERE u.id = ?
+        """, (id, ))
+
+        users = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+
+            user = User(row['id'], row['first_name'], row['last_name'], row['email'], row['bio'], row['username'], row['password'], row['profile_image_url'], row['created_on'], row['active'])
+
+            users.append(user.__dict__)
+
+        return users
