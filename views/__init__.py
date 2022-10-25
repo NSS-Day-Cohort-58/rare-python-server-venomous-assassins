607c867000b0a082b01baa11189e80152ee11a28
Justin Dority <justindority@gmail.com>
2022-10-25 10:17:14 -0500

    can delete your own comments now

--- views/__init__.py
+++ views/__init__.py
@@ -2,5 +2,5 @@ from .user_requests import get_all_users, login_user, create_user
 from .posts_requests import get_all_posts, create_post, get_single_post
 from .categories_request import get_all_categories
 from .tag_requests import get_all_tags
-from .comment_requests import create_comment
+from .comment_requests import create_comment, delete_comment
 from .subscription_requests import delete_subscription, get_all_subscriptions, create_subscription, delete_subscription
